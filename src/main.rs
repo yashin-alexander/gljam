544c667c9031d5ead2868169d0d6be4a009ac7f2
yashin-alexander <alexandr.yashin@emlid.com>
2018-11-19 01:20:56 +0300

    create separate shader files
    - update main.rs to draw a triangle

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
+#![allow(non_upper_case_globals)]
 extern crate glfw;
 use self::glfw::{Context, Key, Action};
 
 extern crate gl;
+use self::gl::types::*;
 
 use std::sync::mpsc::Receiver;
+use std::ptr;
+use std::mem;
+use std::os::raw::c_void;
+
+mod shader;
+
+extern crate cgmath;
+
 
 const SCR_WIDTH: u32 = 640;
 const SCR_HEIGHT: u32 = 480;
 
 
+#[allow(non_snake_case)]
 pub fn main() {
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
     glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
@@ -23,8 +34,69 @@ pub fn main() {
 
     gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
 
+        let (ourShader, VAO) = unsafe {
+        let ourShader = shader::Shader::new(
+            "src/shaders/shader.vs",
+            "src/shaders/shader.fs"
+        );
+
+        // set up vertex data (and buffer(s)) and configure vertex attributes
+        // ------------------------------------------------------------------
+        // HINT: type annotation is crucial since default for float literals is f64
+        let vertices: [f32; 18] = [
+            // positions         // colors
+            0.5, -0.5, 0.0,  1.0, 0.0, 0.0,  // bottom right
+           -0.5, -0.5, 0.0,  0.0, 1.0, 0.0,  // bottom left
+            0.0,  0.5, 0.0,  0.0, 0.0, 1.0   // top
+        ];
+        let (mut VBO, mut VAO) = (0, 0);
+        gl::GenVertexArrays(1, &mut VAO);
+        gl::GenBuffers(1, &mut VBO);
+        // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+        gl::BindVertexArray(VAO);
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, VBO);
+        gl::BufferData(gl::ARRAY_BUFFER,
+                       (vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+                       &vertices[0] as *const f32 as *const c_void,
+                       gl::STATIC_DRAW);
+
+        let stride = 6 * mem::size_of::<GLfloat>() as GLsizei;
+        // position attribute
+        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, stride, ptr::null());
+        gl::EnableVertexAttribArray(0);
+        // color attribute
+        gl::VertexAttribPointer(1, 3, gl::FLOAT, gl::FALSE, stride, (3 * mem::size_of::<GLfloat>()) as *const c_void);
+        gl::EnableVertexAttribArray(1);
+
+        // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+        // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+        // gl::BindVertexArray(0);
+
+        (ourShader, VAO)
+    };
+
+    // render loop
+    // -----------
     while !window.should_close() {
+        // events
+        // -----
         process_events(&mut window, &events);
+
+        // render
+        // ------
+        unsafe {
+            gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+
+            // render the triangle
+            ourShader.useProgram();
+            gl::BindVertexArray(VAO);
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+        }
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
         window.swap_buffers();
         glfw.poll_events();
     }
